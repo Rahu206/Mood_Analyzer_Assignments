b839bb8d9b6857b04a219c3a437381dc26d2bd31
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-29 00:43:37 +0530

    TC 1.2-ability to analyze and respond to happy  mood

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bridgelabz.exception.MoodAnalyser;
+
+public class MoodAnalyserTest {
+	MoodAnalyser moodanalyser = null;
+	
+	@Before
+	public void seUp() {
+		moodanalyser = new MoodAnalyser();
+	}
+	
+	/*@Test
+	public void givenMessageIsSadMood_shouldReturn_Sad() {
+		String mood = moodanalyser.analyseMood("I am in sad mood");
+		Assert.assertEquals("SAD", mood);
+	}*/
+	
+	
+	@Test
+	public void givenMessageIsAnyMood_shouldReturn_Happy() {
+		String mood = moodanalyser.analyseMood("I am in any mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+
+}
