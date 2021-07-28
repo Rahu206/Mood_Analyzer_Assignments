5fca509ab9a0647efe745a48327bcf620805392b
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-29 01:46:19 +0530

    UC2-added method by considering NullException

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.exception.MoodAnalyser;
+
+public class MoodAnalyserTest {
+
+	@Test
+	public void givenMessageIsSadMood_shouldReturn_Sad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in sad mood");
+		// String mood = moodanalyser.analyseMood();
+		Assert.assertEquals("SAD", moodanalyser.analyseMood());
+	}
+
+	@Test
+	public void givenMessageNotSad_shouldReturn_Happy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in any mood");
+		// String mood = moodanalyser.analyseMood();
+		Assert.assertEquals("HAPPY", moodanalyser.analyseMood());
+	}
+
+	@Test
+	public void givenMessageIsNullMood_shouldReturn_Happy() {
+		try {
+			MoodAnalyser moodanalyser = new MoodAnalyser(null);
+			// String mood = moodanalyser.analyseMood();
+			Assert.assertEquals("HAPPY", moodanalyser.analyseMood());
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
