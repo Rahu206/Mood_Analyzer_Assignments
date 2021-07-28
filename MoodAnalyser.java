d5deaff4e3f310d7c1d546df6fcaa6dab1bf17fd
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-29 01:24:05 +0530

    Refactor code for Moodanalysis

---
+++ MoodAnalyser.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.exception;
+
+public class MoodAnalyser {
+	private String message;
+
+	public MoodAnalyser() {
+		super();
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+
+	public String analyseMood() {
+		if (message.contains("sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
+
+}
