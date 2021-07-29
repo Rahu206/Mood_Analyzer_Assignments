e8e4bd39e094868e573e6f6e9138bb19921e1ad8
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-29 20:31:23 +0530

    UC3-added: handling the custom exception if user entered invalid mood

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,54 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.exception.MoodAnalyser;
+import com.bridgelabz.exception.MoodAnalysisException;
+
+public class MoodAnalyserTest {
+	@Test
+	public void givenMessageIsSadMood_shouldReturn_Sad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in sad mood");
+		String mood = null;
+		try {
+			mood = moodanalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessageNotSad_shouldReturn_Happy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in any mood");
+		String mood = null;
+		try {
+			mood = moodanalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void givenMessageIsNullMood_should_ThrowException() {
+		MoodAnalyser moodanalyser = new MoodAnalyser(null);
+		try {
+			moodanalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.expectedType.Null_EXCEPTION,e.type);
+		}
+	}
+
+	@Test
+	public void givenMessageIsEmpty_should_ThrowException() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("");
+		try {
+			moodanalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.expectedType.EMPTY_EXCEPTION,e.type);
+		}
+	}
+
+}
